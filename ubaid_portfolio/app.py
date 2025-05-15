27d5c600e467dc2581b857ed4b7209eb4ec6db83
ubaid <ubaidsaleem825@gmail.com>
2025-05-15 07:57:05 +0500

    database setup

--- ubaid_portfolio/app.py
+++ ubaid_portfolio/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+import psycopg2
 
 app = Flask(__name__)
+# Connect to database
+conn = psycopg2.connect(host='localhost', port='5432',user ='postgres',password='ubaid321', database='ubaid_portfolio')
 
-@app.route('/')
+# cursor setup
+cur = conn.cursor()
+
+# Define url
+@app.route('/',methods=['GET','POST'])
 def Home():
-    return render_template('index.html')
+    try:
+        if request.method == 'POST':
+            Name = request.form['name']
+            Email = request.form['email']
+            Subject = request.form['subject']
+            Message = request.form['message']
+    except:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
